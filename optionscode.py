88d84ce379d9ebb87741fe483e098e0b26f6754c
kabitagautam <kabita.gautam121@gmail.com>
2021-01-27 16:16:14 +0100

    optioncodes.py

---
+++ optionscode.py
@@ -0,0 +1,14 @@
+print("Option 1")
+print("Option 2")
+print("Option 3")
+c=input("Type the option")
+print(c)
+if c=="1":
+  print("You selected 1")
+  MyfunctionSine(100,1)
+if c=="2":
+  print("You selected 2")
+  PlotLetter("test MODULE 3")
+if c=="3":
+  print("You selected 3")
+  print("Exit")
